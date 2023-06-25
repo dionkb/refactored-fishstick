e278fe926f9c99e6fd4f71d93e828fc4e4b28e2f
dionkb <dionkbaskara@gmail.com>
2023-06-25 19:53:58 -0400

    added nested document syntax and finished more TODO in models

--- models/Thought.js
+++ models/Thought.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const reactionSchema = new mongoose.Schema({
     reactionId: { 
         type: mongoose.Schema.Types.ObjectId, 
-        // default: TODO: 
+        default: () => new Types.ObjectId(),
     },
     reactionBody: { 
         type: String, 
@@ -19,37 +19,61 @@ const reactionSchema = new mongoose.Schema({
     createdAt: {
         type: Date, 
         default: Date.now,
-        // TODO: Add 'getter'?? method
+        // get: timeFormat, TODO:
     }
 });
 
 // Construct a new instance of the schema class
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: { 
-        type: String, 
-        required: true, 
-        minLength: 1,
-        maxLength: 280
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: { 
+            type: String, 
+            required: true, 
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: { 
+            type: Date, 
+            default: Date.now,
+            // get: timeFormat, TODO:
+        },
+        username: { 
+            type: String, 
+            required: true, 
+        },
+        reactions: [reactionSchema],
+        // Use built in date method to get current date
+        lastAccessed: { 
+            type: Date, 
+            default: Date.now 
+        },
     },
-    createdAt: { 
-        type: Date, 
-        default: Date.now,
-        // TODO: Add 'getter'?? method
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     },
-    username: { 
-        type: String, 
-        required: true, 
-    },
-    reactions: [reactionSchema],
-    // Use built in date method to get current date
-    lastAccessed: { 
-        type: Date, 
-        default: Date.now 
-    },
-});
+);
 
-// Using mongoose.model() to compile a model based on the schema 'bookSchema'
-// const Thought = mongoose.model('Thoughts', thoughtSchema);
+// TODO: Creating the getter function used in createdAt attribute of reactionSchema & thoughtSchema
+// function timeFormat(date) {
+//     if (date) {
+//         return date      
+//     }
+// }
+
+// Adding a 'virtual' property to the model
+thoughtSchema
+    // naming the virtual function
+    .virtual('reactionCount')
+    // The GETTER function
+    .get(function() {
+        return this.reactions.length;
+    });
+
+// Using mongoose.model() to compile a model based on the schema 'thoughtSchema'
+const Thought = mongoose.model('Thoughts', thoughtSchema);
 
 // Create new instances of the model, a document
 // Thought.create([
